d2c57e60178a417acb7490a8f94c60577c7d6ff1
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-06-29 19:05:22 +0900

    doge

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+import collections as coll
 import argparse
 import logging as lo
 import typing as t
@@ -15,6 +16,7 @@ def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("company_file")
     parser.add_argument("daily_report_dir")
+    parser.add_argument("output")
     return parser
 
 
@@ -71,8 +73,124 @@ def find_funding_rounds(
     )
 
 
+def find_active_investors(disruptors, investments):
+    """
+    Parameters
+    ----------
+    disruptors: dict
+        '<UUID>':
+    """
+    funding_round_uuids = [
+        funding_round["uuid"]
+        for disruptor_uuid, disruptor in disruptors.items()
+        for funding_round in disruptor["funding_rounds"]
+    ]
+
+    active_investors = dict(
+        (investment["investor_uuid"], investment["investor_name"])
+        for investment in investments
+        if investment["funding_round_uuid"] in funding_round_uuids
+        and investment["investor_type"] == "organization"
+    )
+    return active_investors
+
+
+def find_investor_funding_round_uuids(active_investors, investments):
+    """
+    """
+    return dict(
+        (
+            uuid,
+            {
+                "name": name,
+                "funding_round_uuids": [
+                    investment["funding_round_uuid"]
+                    for investment in investments
+                    if investment["investor_uuid"] == uuid
+                ],
+            },
+        )
+        for uuid, name in active_investors.items()
+    )
+
+
+def find_investor_funding_rounds(investor_funding_round_uuids, funding_rounds):
+    """
+    """
+    funding_round_uuids = set(
+        uuid
+        for _, investor in investor_funding_round_uuids.items()
+        for uuid in investor["funding_round_uuids"]
+    )
+    active_funding_rounds = dict(
+        (funding_round["uuid"], funding_round)
+        for funding_round in funding_rounds
+        if funding_round["uuid"] in funding_round_uuids
+    )
+    return dict(
+        (
+            investor_uuid,
+            {
+                "name": investor["name"],
+                "funding_rounds": [
+                    active_funding_rounds[uuid]
+                    for uuid in investor["funding_round_uuids"]
+                ],
+            },
+        )
+        for investor_uuid, investor in investor_funding_round_uuids.items()
+    )
+
+
+def find_funding_round_types(funding_rounds):
+    return list(
+        set(
+            funding_round["investment_type"]
+            for funding_round in funding_rounds
+        )
+    )
+
+
+def write_report(investor_funding_rounds, funding_round_types, output):
+    """
+    """
+    with open(output, "w") as stream:
+        writer = csv.writer(stream)
+
+        writer.writerow(
+            ["active investor uuid", "name"]
+            + funding_round_types
+            + ["all types"]
+        )
+
+        for uuid, investor in investor_funding_rounds.items():
+            fundamental = [uuid, investor["name"]]
+            funding_rounds = investor["funding_rounds"]
+            count = coll.Counter(
+                [
+                    funding_round["investment_type"]
+                    for funding_round in funding_rounds
+                ]
+            )
+            per_type = [
+                count.get(funding_round_type, 0)
+                for funding_round_type in funding_round_types
+            ]
+            writer.writerow(fundamental + per_type + [len(funding_rounds)])
+
+
 if __name__ == "__main__":
     options = parse(sys.argv[1:])
     companies = read_companies(options.company_file)
-    funding_rounds = read_fundinging_rounds(options.funding_rounds)
-    current = find_funding_rounds(companies, funding_rounds)
+    funding_rounds = read_csv(options.funding_rounds)
+    investments = read_csv(options.investments)
+    disruptors = find_funding_rounds(companies, funding_rounds)
+    active_investors = find_active_investors(disruptors, investments)
+    investor_funding_round_uuids = find_investor_funding_round_uuids(
+        active_investors, investments
+    )
+    investor_funding_rounds = find_investor_funding_rounds(
+        investor_funding_round_uuids, funding_rounds
+    )
+    funding_round_types = find_funding_round_types(funding_rounds)
+    write_report(investor_funding_rounds, funding_round_types, options.output)
